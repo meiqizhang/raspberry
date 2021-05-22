549c2355250c2c8d9d9f97a876a3d68326b57709
meiqizhang <zhangqi_gsts@qq.com>
2021-05-22 23:55:33 +0800

    update show info

--- show_info.py
+++ show_info.py
@@ -16,25 +16,32 @@ from luma.oled.device import ssd1306, ssd1325, ssd1331, sh1106
 
 
 def get_ip_address(ifname):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    return socket.inet_ntoa(fcntl.ioctl(
-        s.fileno(),
-        0x8915, # SIOCGIFADDR
-        struct.pack('256s', ifname[:15].encode())
-    )[20:24])
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        return socket.inet_ntoa(fcntl.ioctl(
+            s.fileno(),
+            0x8915, # SIOCGIFADDR
+            struct.pack('256s', ifname[:15].encode())
+        )[20:24])
+    except Exception as e:
+        return "unknow"
 
 
 # rev.1 users set port=0
 # substitute spi(device=0, port=0) below if using that interface
 
 def main():
-    cmd = "ps -ef | grep 'show_info.py' | grep -v 'grep' | awk '{print $2}'"
-    p=subprocess.Popen(cmd, shell=True,stdout=subprocess.PIPE)
+    cmd = "ps -ef | grep 'show_info.py' | grep -v 'grep' | grep python3 | awk '{print $2}'"
+    p = subprocess.Popen(cmd, shell=True,stdout=subprocess.PIPE)
     output = p.stdout.readlines()
     pids = [int(x.decode()) for x in output]
     pids.remove(os.getpid())
+    ppid = os.getppid()
+    if ppid in pids:
+        pids.remove(os.getppid())
     if len(pids) > 0:
         print("already running...")
+        print(pids)
         return
 
     while True:
@@ -52,4 +59,5 @@ def main():
 
 
 if __name__ == "__main__":
+    print(get_ip_address("wlan0"))
     main()
